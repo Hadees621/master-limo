bf0f56f1f216716fa14186b9a0961431ad31840e
hadees <hadees621@gmail.com>
2024-05-12 13:39:55 +0500

    fix: contact fix

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,3 +1,4 @@
+import ContactForm from "@/components/contact/ContactForm";
 import { leagueSpartan700, libre400, montserrat400 } from "@/util/fonts";
 import React from "react";
 
@@ -72,78 +73,9 @@ function Contact() {
                 </h6>
               </div>
             </div>
-            <div class="mx-auto px-0 md:px-14 lg:px-36">
-              <form className="bg-black grid space-y-12">
-                <div class="relative mb-6 flex flex-col">
-                  <input
-                    type="text"
-                    className="w-full border-b bg-transparent pb-4"
-                    id="exampleInput90"
-                    placeholder="Topic"
-                  />
-                </div>
-                <div class="relative mb-6 flex flex-col justify-between xs:flex-row">
-                  <div className="mb-3 flex flex-col xs:mb-0 xs:w-[48%]">
-                    <input
-                      type="text"
-                      className="w-full border-b bg-transparent pb-4"
-                      id="exampleInput90"
-                      placeholder="First Name *"
-                    />
-                  </div>
-                  <div className="mb-3 flex flex-col xs:mb-0 xs:w-[48%]">
-                    <input
-                      type="text"
-                      className="w-full border-b bg-transparent pb-4"
-                      id="exampleInput90"
-                      placeholder="Last Name *"
-                    />
-                  </div>
-                </div>
-                <div class="relative mb-6 flex flex-col justify-between xs:flex-row">
-                  <div className="mb-3 flex flex-col xs:mb-0 xs:w-[48%]">
-                    <input
-                      type="text"
-                      className="w-full border-b bg-transparent pb-4"
-                      id="exampleInput90"
-                      placeholder="Phone *"
-                    />
-                  </div>
-                  <div className="mb-3 flex flex-col xs:mb-0 xs:w-[48%]">
-                    <input
-                      type="text"
-                      className="w-full border-b bg-transparent pb-4"
-                      id="exampleInput90"
-                      placeholder="Email *"
-                    />
-                  </div>
-                </div>
-                <div class="relative mb-6 flex">
-                  <div className="flex w-full flex-col">
-                    <div className="flex flex-col">
-                      <p className="pe-4 text-white xxlg:text-xl">Message</p>
-                      <textarea
-                        name=""
-                        id=""
-                        cols="30"
-                        rows="4"
-                        className="border-b bg-transparent"
-                      ></textarea>
-                    </div>
-                  </div>
-                </div>
-                <button
-                  type="button"
-                  data-te-ripple-init
-                  data-te-ripple-color="light"
-                  class="hover:bg-primary-600 focus:bg-primary-600 active:bg-primary-700 mt-10 inline-block w-full rounded bg-[#2189B7] px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] lg:mb-0"
-                >
-                  Send
-                </button>
-              </form>
-            </div>
+            <ContactForm />
           </div>
-        </div>
+        </div>  
       </section>
     </div>
   );
