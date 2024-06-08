b9bf06cd79fc0995fb1e290e1b8880c0df581638
hadees <hadees621@gmail.com>
2024-06-08 16:02:25 +0500

    fix

--- src/app/reservation/page.js
+++ src/app/reservation/page.js
@@ -18,22 +18,15 @@ function Reservation() {
   return (
     <div className="flex w-full items-center justify-center">
       <div className="w-full max-w-[1796px] px-5 py-24 md:px-20 md:py-32 lg:px-12 lg:py-32 xl:px-20 xl:py-40 2xl:py-32">
-        <BackgroundBanner />
-        <div className="flex w-full justify-center ">
+        {/* <BackgroundBanner /> */}
+        <div className="flex w-full justify-center">
           <a
-            className="cursor-pointer"
             href="https://book.mylimobiz.com/v4/masterpiecelimo"
             data-ores-widget="website"
             data-ores-alias="masterpiecelimo"
           >
             Online Reservations
           </a>
-          {/* <script
-            type="text/javascript"
-            src="https://book.mylimobiz.com/v4/widgets/widget-loader.js"
-          >
-          </script>
-          */}
         </div>
       </div>
     </div>
--- src/components/reservation/BackgroundBanner.js
+++ src/components/reservation/BackgroundBanner.js
@@ -8,7 +8,7 @@ function BackgroundBanner() {
       src="https://res.cloudinary.com/ozecloud/image/upload/q_auto:best/v1700973067/f966bb205f8eea68663d865c0868bd02_m4ehqs.webp"
       alt=""
       layout="fill"
-      className="absolute left-0 top-0 -z-10 h-full w-full object-cover object-top"
+      className="h-full w-full object-cover object-top"
     />
   );
 }
